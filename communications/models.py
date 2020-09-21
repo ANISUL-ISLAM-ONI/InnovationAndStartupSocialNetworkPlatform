22b2e715e09678dcedb54d0ce605a97ae62cc1d0
EkhtiarUddin <uddinikhtiar6@gmail.com>
2020-09-21 23:34:08 +0600

    update

--- communications/models.py
+++ communications/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  owner = models.ForeignKey(
+    User,
+    default=1,
+    on_delete=models.CASCADE,
+    null=True
+  )
+ class Comment(models.Model):
+  text = models.TextField(default="awesome!")
+  post = models.ForeignKey(
+    Post,
+    default=1,
+    on_delete=models.CASCADE
+  )
+  owner = models.ForeignKey(
+    User,
+    default=1,
+    on_delete=models.CASCADE
+  )

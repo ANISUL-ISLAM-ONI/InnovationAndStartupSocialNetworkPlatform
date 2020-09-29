50c716f7347afa3f30244c88cf8c88b48d93ab33
Shammo103 <18101103@uap-bd.edu>
2020-09-29 21:24:40 +0600

    edit by Farah

--- InnovationAndStartupSocialNetworkPlatform/urls.py
+++ InnovationAndStartupSocialNetworkPlatform/urls.py
@@ -18,10 +18,13 @@ from django.urls import path, include
 from django.contrib import admin
 from django.urls import path, include
 from Profile import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls'), name='account_app'),
     path('profile/', views.profile),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
